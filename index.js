66e90960f3471d62ed8c217bde9a57141353e65d
Jonathan Meguira <jonathan@herolo.co.il>
2018-01-15 22:34:11 +0200

    modified greeting processes

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const request = require('request');
-var admin = require("firebase-admin");
 const port = process.env.PORT || 5000;
-var serviceAccount = require("./keys.json");
 const messager = require('./lib/message-generator');
-const user = 'Jonathan';
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://gigzr-f2a9e.firebaseio.com"
-});
+const owner = require('./lib/owner');
+const FBAPP = require('./lib/firebase.init');
 
 var app = express();
 
@@ -34,21 +29,23 @@ app.get('/', function (request, response) {
     response.render('pages/index');
 });
 
-var db = admin.database();
-
 app.get('/', (req, res) => {
 	res.send('salut');
 })
 
 app.post('/checkId', function (req, res) {
 	const body = req.body;
-	console.log(req);
 	const uid = body['messenger user id'];
-	db.ref("owners/" + uid).once('value').then((snapshot) => {
+	FBAPP.FireDB.ref("owners/" + uid).once('value').then((snapshot) => {
 		const user = snapshot.val() || 'undefined';
 		if (user !== 'undefined') {
 			const response = {
 				"redirect_to_blocks": ["known_user"]
+			}
+			res.send(response);
+		} else {
+			const response = {
+				"redirect_to_blocks": ["INTRO"]
 			}
 			res.send(response);
 		}
@@ -57,36 +54,13 @@ app.post('/checkId', function (req, res) {
 
 app.post('/saveOwner', (req, res) => {
 	const body = req.body;
-	const uid = body['messenger user id'];
-	const timeNow = Date.now();
-	let allGigs;
-	const gig = { location: body['gigr_address'], need: body['gigr_need'], quantity: body['gigr_qantity'], time: body['gigr_when'] };
-	db.ref("owners/" + uid).once('value').then((snapshot) => {
-		const owner = snapshot.val() || 'undefined';
-		if (owner !== 'undefined') {
-			allGigs = owner.gigs
-			allGigs.push(gig);
-			db.ref('owners/' + uid).update({
-				gigs: allGigs
-			})
-		} else {
-			let gigs = [];
-			gigs.push(gig);
-			db.ref("owners/" + uid).set({
-				uid: uid,
-				phone: body['gigr_phone'],
-				restaurant: body['gigr_address'],
-				gigs: gigs
-			})
-		}
-	}, err => console.log(err));
-
+	owner.updateOwnerGigs(body);
 })
 
 app.post('/saveWorker', (req, res) => {
 	const body = req.body;
 	const uid = body['messenger user id'];
-	db.ref("workers/" + uid).set({
+	FBAPP.FireDB.ref("workers/" + uid).set({
 		uid: uid,
 		firstName: body['first name'],
 		lastName: body['last name'],
@@ -99,12 +73,11 @@ app.post('/saveWorker', (req, res) => {
 app.post('/lastOrders', (req, res) => {
 	const body = req.body;
 	const uid = body['messenger user id'];
-	db.ref("owners/" + uid).once('value').then((snapshot) => {
+	FBAPP.FireDB.ref("owners/" + uid).once('value').then((snapshot) => {
 		const user = snapshot.val() || 'undefined';
 		if (user !== 'undefined') {
 			const gigLen = user.gigs.length - 1;
 			const lastGig = user.gigs[gigLen];
-			console.log(JSON.stringify(lastGig))
 			const text = `Avez vous toujours besoin de ${lastGig.need} a ${lastGig.location} ?`
 			const buttons = [
 				{
@@ -122,6 +95,11 @@ app.post('/lastOrders', (req, res) => {
 			res.send(message);
 		}
 	})
+})
+
+app.post('/updateOwner', (req, res) => {
+	const body = req.body;
+	owner.updateOwnerGigs(body);
 })
 
 app.listen(port, (err) => {
